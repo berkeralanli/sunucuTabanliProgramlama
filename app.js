576e20dee450dd1c6cb2973bc5846de204d1702a
berkeralanli <147711608+berkeralanli@users.noreply.github.com>
2024-01-12 13:07:04 +0300

    final

--- app.js
+++ app.js
@@ -24,60 +24,119 @@ dbConn.connect((err)=>{
 })
 
 //register fonksiyonu olusturma 
-const register=async(req,res) => {
+const musteri_ekle=async(req,res) => {
   const username=req.body.username;
   const email=req.body.email;
   const password= await bcrypt.hash(req.body.password,10);
+  
+
   dbConn.query("INSERT INTO user (username, email, password) VALUES (?,?,?)", [username,email,password], (err,results) => {
-  if (err){
-    console.log(err)
-  }else{
-    return res.status(201).json({
-      success:true,
-      message:'kayit basarili'
-    });
-  }
-});
-};
+    if (err){
+      console.log(err)
+    }else{
+      return res.status(201).json({
+        success:true,
+        message:'kayit basarili'
+      });
+    }
+  });
+  };
 
 //login fonksiyonun olusturulmasi
 const login=async(req,res) =>{
   const username=req.body.username;
   const password=req.body.password;
   //database username varliginin sorgusu > var ise sifre ve sonucun.sifresinin karsilastirilmasi
-  dbConn.query("SELECT * FROM user WHERE username=?",[username],(error,results)=>{
+  dbConn.query("SELECT * FROM user WHERE username=?",[username],(err,results)=>{
     if(results.length>0){
       //bcryptin'compare fonksiyonu kullanarak karsilastirma yapilmasi
       const comparePassword=bcrypt.compare(password,results[0].password)
       //bcrpyt olmadan karsilastirma yapilmak istenirse asagidaki duz string karsilastirmasi yapilabilir === 
-      //if(password === results[0].password){}
+        //if(password === results[0].password){}
 
-      if(comparePassword){
-        return res.status(203).json({
-          success:true,
-          message:"Giris basarili"
-        })
+        if(comparePassword){
+          return res.status(203).json({
+            success:true,
+            message:"Giris basarili"
+          })
+        }else{
+          return res.status(203).json({
+            success:false,
+            message:"Giris basarisiz"
+          })
+        }
       }else{
         return res.status(203).json({
           success:false,
-          message:"Giris basarisiz"
+          message:"Kullanici adi veya sifre hatali"
         })
       }
-    }else{
-      return res.status(203).json({
-        success:false,
-        message:"Kullanici adi veya sifre hatali"
-      })
-    }
+    })
+};
+const musteri_getir=(req,res)=>{
+  dbConn.query("SELECT username,password,eposta, FROM user",(error,result)=>{
+      if(error){
+          console.log("Sunucu yanıt vermiyor")
+          return new Response().error500(res)
+      }else{
+          res.json({data:result})
+      }
   })
 };
+const musteri_guncelle=async(req,res)=>{
+  const eposta=req.params.eposta
+  const username=req.body.username
+  const password=req.body.password
+
+ 
+      dbConn.query("UPDATE user SET username=?,password=? WHERE eposta=?",
+      [username,password,eposta],(error,result)=>{
+          if(error){
+              console.log(error)
+              return new Response().error500(res)
+          }else{
+              return new Response().kayit_guncelle(res)
+              location.reload()
+          }
+      })  
+}
+
+const musteri_sil=async(req,res)=>{
+  const eposta=req.params.eposta
+  dbConn.query("DELETE FROM user WHERE eposta=?",[eposta],(error,result)=>{
+    if(error){
+      console.log("Sunucu yanıt vermiyor")
+      return new Response().error500(res)
+  }else{
+      res.json({data:result})
+  }
+  })
+}
 
 //router ve app kavramlari
-router.post("/register",register); //varolan degiskenin atandigi bir endpoint
-router.post("/login",login);  //varolan degiskenin atandigi bir endpoint
-app.get('/endpoint', (req,res) => {
-  res.send('merhaba bu YENI bir endpoint');
-});
+router.post("/musteri_ekle",musteri_ekle); //varolan degiskenin atandigi bir endpoint
+router.post("/login",login);
+router.get("/musteri_getir",musteri_getir);
+router.delete("/musteri_sil/:eposta",musteri_sil);
+router.put("/musteri_guncelle/:eposta",musteri_guncelle);
+
+
+// sayfa linkleri
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,"index.html"))
+})
+app.get('/musteri_ekle', function (req, res) {
+  res.sendFile(path.join(__dirname,"musteri_ekle.html"))
+})
+app.get('/musteri_sil', function (req, res) {
+  res.sendFile(path.join(__dirname,"musteri_sil.html"))
+})
+app.get('/musteri_guncelle', function (req, res) {
+  res.sendFile(path.join(__dirname,"musteri_guncelle.html"))
+})
+app.get('/musteri_getir', function (req, res) {
+  res.sendFile(path.join(__dirname,"musteri_getir.html"))
+})
 
 
 
@@ -87,5 +146,4 @@ const PORT = 3000;
 app.use(express.json({limit:'50mb',extended:true,parameterLimit:50000}));
 app.use('/api',router);
 app.listen(PORT);
-
-module.exports={register,dbConn,login};
+module.exports={musteri_ekle,dbConn,login,musteri_getir,musteri_sil,musteri_guncelle};
